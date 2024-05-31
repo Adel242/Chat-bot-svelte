fe1c9a5d6ae78c723258e744923fc17a0861cb8a
gustavoespindola <espindolage@gmail.com>
2024-05-31 11:17:25 -0400

    minor menu design improve

--- src/components/menu.svelte
+++ src/components/menu.svelte
@@ -5,6 +5,7 @@
 	import { credentials } from '../stores/credentials-store'
 	import { toast } from 'svelte-sonner'
 	import { goto } from '$app/navigation'
+	import { loading } from '../stores/loading-store'
 
 	export let apiKey
 
@@ -21,20 +22,32 @@
 
 {#if apiKey}
 	<div class="dropdown">
-		<button class="btn my-1" tabindex="0">
-			<img src={$user?.avatar_url} alt={$user?.full_name} class="w-6 h-6 rounded-full" />
-			<div class="ml-2">{$user?.full_name}</div>
+		<button
+			class={`btn btn-circle btn-outline bg-transparent p-0`}
+			tabindex="0"
+			style="background-color: transparent;"
+		>
+			<div class="flex items-center gap-2 w-8 h-8 rounded-full">
+				{#if $user?.avatar_url}
+					<img src={$user?.avatar_url} alt="user avatar" class="w-8 h-8 rounded-full" />
+				{/if}
+			</div>
 		</button>
-		<div class="dropdown-menu dropdown-menu-bottom-left origin-top-right w-[100%]">
+
+		<!-- dropdown-item -->
+		<div class="dropdown-menu dropdown-menu-bottom-left origin-top-right min-w-40 w-[100%]">
 			<a
-				href="https://staging.codegpt.co/es"
+				href="https://app.codegpt.co"
 				class="dropdown-item text-sm flex justify-end items-start"
 				target="_blank"
+				tabindex="-1"
 				rel="noopener noreferrer">Go to Code GPT</a
 			>
-			<button
-				class="dropdown-item text-sm flex justify-end items-start dropdown-divider"
-				on:click={handleSignOut}>Sign out</button
+			<a
+				href="/settings"
+				tabindex="-1"
+				class="dropdown-item text-sm flex justify-end items-start"
+				on:click={handleSignOut}>Sign out</a
 			>
 		</div>
 	</div>
