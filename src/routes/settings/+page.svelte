dd17404aa9336bc6b45006320cd6d332e80a4cf8
gustavoespindola <espindolage@gmail.com>
2024-06-05 11:26:37 -0400

    add help text to login

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -39,11 +39,9 @@
 </script>
 
 <main class="flex flex-col h-full items-center justify-center">
-	<div class="max-w-md m-auto grid gap-2">
-		<section class="w-full">
+	<div class="w-full sm:max-w-md m-auto grid gap-2 justify-center">
+		<section class="grid gap-2">
 			<h1 class="font-bold">Sign In</h1>
-		</section>
-		<section>
 			<form class="card" on:submit|preventDefault={handleSubmit}>
 				<div class="card-body">
 					<div class="grid gap-2">
@@ -76,6 +74,30 @@
 					</div>
 				</div>
 			</form>
+		</section>
+		<section>
+			<p class="flex text-xs text-gray-12 gap-2 items-center justify-between px-2">
+				<a href="https://app.codegpt.co/en/apikeys" target="_blank">
+					Get your Api key <u>here</u>
+				</a>
+				<a
+					href="https://intercom.help/codegpt/en/articles/9424841-where-do-i-find-my-codegpt-api-key"
+					target="_blank"
+					class="flex items-center gap-1"
+				>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						height="16px"
+						width="16px"
+						viewBox="0 -960 960 960"
+						fill="#5f6368"
+						><path
+							d="M440-280h80v-240h-80v240Zm40-320q17 0 28.5-11.5T520-640q0-17-11.5-28.5T480-680q-17 0-28.5 11.5T440-640q0 17 11.5 28.5T480-600Zm0 520q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"
+						/></svg
+					>
+					Help
+				</a>
+			</p>
 		</section>
 	</div>
 </main>
